21064d71a06f67915a68a8c00214884e71941bfd
BVSidhartha <70025840+BVSidhartha@users.noreply.github.com>
2025-09-26 12:25:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-
-st.set_page_config(page_title="Sanity Test", page_icon="✅")
-
+st.set_page_config(page_title="Sanity", page_icon="✅")
 st.title("✅ Streamlit is working")
-st.write("If you can see this page, your deployment is fine.")
+st.write("If you can see this page, the platform is fine.")
 
